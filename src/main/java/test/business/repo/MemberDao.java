4bbbd04ecd4d9e6a2d42226e378d137d0cd4e175
schan9788 <scahn9784@gmail.com>
2023-07-20 00:12:59 +0900

    first commit

---
+++ src/main/java/test/business/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package test.business.repo;
+
+import java.util.List;
+
+import test.business.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
